580cb4d8d5abc905c0c38d567f9d2ca16ea9d13e
hilderonny <hilderonny2014@gmail.com>
2026-02-20 13:07:39 +0100

    API tasks/list

--- api/tasks/handler.go
+++ api/tasks/handler.go
@@ -1,15 +1,40 @@
 package tasks
 
 import (
+	"encoding/json"
 	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"time"
 )
 
+type TaskStruct struct {
+	Id          string
+	Type        string
+	File        string
+	Status      string
+	Progress    string
+	CreatedAt   time.Time
+	StartedAt   time.Time
+	CompletedAt time.Time
+	Worker      string
+}
+
 var FILES_ROOT string
+var TASKS []TaskStruct = []TaskStruct{}
 
 func Add(responseWriter http.ResponseWriter, request *http.Request) {
 }
 
 func Complete(responseWriter http.ResponseWriter, request *http.Request) {
+}
+
+func Cleanup() {
+	directoryEntries, _ := os.ReadDir(FILES_ROOT)
+	for _, directoryEntry := range directoryEntries {
+		os.RemoveAll(path.Join([]string{FILES_ROOT, directoryEntry.Name()}...))
+	}
 }
 
 func Details(responseWriter http.ResponseWriter, request *http.Request) {
@@ -19,6 +44,21 @@ func File(responseWriter http.ResponseWriter, request *http.Request) {
 }
 
 func List(responseWriter http.ResponseWriter, request *http.Request) {
+	filteredTasks := make([]map[string]string, len(TASKS))
+	for i, task := range TASKS {
+		filteredTask := make(map[string]string)
+		filteredTask["id"] = task.Id
+		filteredTask["type"] = task.Type
+		filteredTask["file"] = task.File
+		filteredTask["status"] = task.Status
+		filteredTask["progress"] = task.Progress
+		filteredTask["createdat"] = strconv.FormatInt(task.CreatedAt.UnixNano(), 10)
+		filteredTask["startedat"] = strconv.FormatInt(task.StartedAt.UnixNano(), 10)
+		filteredTask["completedat"] = strconv.FormatInt(task.CompletedAt.UnixNano(), 10)
+		filteredTask["worker"] = task.Worker
+		filteredTasks[i] = filteredTask
+	}
+	json.NewEncoder(responseWriter).Encode(filteredTasks)
 }
 
 func Progress(responseWriter http.ResponseWriter, request *http.Request) {
@@ -47,6 +87,9 @@ func WorkerStatistics(responseWriter http.ResponseWriter, request *http.Request)
 
 func Register(filesRoot string) {
 	FILES_ROOT = filesRoot
+	// Dateiverzeichnis nach Neustart bereinigen, falls da altes Zeugs drin liegt
+	Cleanup()
+	// API Routen registrieren
 	http.HandleFunc("POST /api/tasks/add", Add)
 	http.HandleFunc("POST /api/tasks/complete/{taskid}", Complete)
 	http.HandleFunc("GET /api/tasks/details/{taskid}", Details)
